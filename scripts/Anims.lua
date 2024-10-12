4bb2230d6ba7df7b6434dc99e3d4d7ebe42d03d9
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-10-11 19:18:13 -0700

    Animation framework
    - Added animation script framework

---
+++ scripts/Anims.lua
@@ -0,0 +1,73 @@
+-- Required script
+require("lib.GSAnimBlend")
+local pose = require("scripts.Posing")
+
+-- Animations setup
+local anims = animations.FurretTaur
+
+--[[
+-- Parrot pivots
+local parrots = {
+	
+	parts.group.LeftParrotPivot,
+	parts.group.RightParrotPivot
+	
+}
+--]]
+
+-- Calculate parent's rotations
+local function calculateParentRot(m)
+	
+	local parent = m:getParent()
+	if not parent then
+		return m:getTrueRot()
+	end
+	return calculateParentRot(parent) + m:getTrueRot()
+	
+end
+
+function events.TICK()
+	
+	-- Animation states
+	local isPose     = true
+	local groundIdle = false
+	
+	-- Animations
+	anims.pose:playing(isPose)
+	anims.ground_idle:playing(groundIdle)
+	
+end
+
+function events.RENDER(delta, context)
+	
+	--[[
+	-- Parrot rot offset
+	for _, parrot in pairs(parrots) do
+		parrot:rot(-calculateParentRot(parrot:getParent()) - vanilla_model.BODY:getOriginRot())
+	end
+	--]]
+	
+end
+
+-- GS Blending Setup
+local blendAnims = {
+	{ anim = anims.pose,        ticks = {7,7} },
+	{ anim = anims.ground_idle, ticks = {7,7} }
+}
+
+-- Apply GS Blending
+for _, blend in ipairs(blendAnims) do
+	blend.anim:blendTime(table.unpack(blend.ticks)):onBlend("easeOutQuad")
+end
+
+-- Fixing spyglass jank
+function events.RENDER(delta, context)
+	
+	--[[
+	local rot = vanilla_model.HEAD:getOriginRot()
+	rot.x = math.clamp(rot.x, -90, 30)
+	parts.group.Spyglass:offsetRot(rot)
+		:pos(pose.crouch and vec(0, -4, 0) or nil)
+	--]]
+	
+end
