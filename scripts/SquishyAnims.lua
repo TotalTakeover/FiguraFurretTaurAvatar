886d028e93f4cce93890131d509323058406a355
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-10-12 01:53:46 -0700

    Create SquishyAnims.lua

---
+++ scripts/SquishyAnims.lua
@@ -0,0 +1,196 @@
+-- Required scripts
+local parts  = require("lib.PartsAPI")
+local squapi = require("lib.SquAPI")
+local lerp   = require("lib.LerpAPI")
+local pose   = require("scripts.Posing")
+
+-- Animation setup
+local anims = animations.FurretTaur
+
+-- Config setup
+config:name("FurretTaur")
+local armsMove = config:load("SquapiArmsMove") or false
+
+-- Lerp tables
+local leftArmLerp  = lerp:new(0.5, armsMove and 1 or 0)
+local rightArmLerp = lerp:new(0.5, armsMove and 1 or 0)
+
+-- Squishy ears
+local ears = squapi.ear:new(
+	parts.group.LeftEar,
+	parts.group.RightEar,
+	0.25,  -- Range Multiplier (0.25)
+	false, -- Horizontal (false)
+	2,     -- Bend Strength (2)
+	true,  -- Do Flick (true)
+	400,   -- Flick Chance (400)
+	0.1,   -- Stiffness (0.1)
+	0.9    -- Bounce (0.9)
+)
+
+-- Tails table
+local tailParts = parts:createChain("Tail")
+
+-- Squishy tail
+local tail = squapi.tail:new(
+	tailParts,
+	20,   -- Intensity X (20)
+	2.5,  -- Intensity Y (2.5)
+	0.8,  -- Speed X (0.8)
+	1,    -- Speed Y (1)
+	2,    -- Bend (2)
+	-1,   -- Velocity Push (-1)
+	0,    -- Initial Offset (0)
+	0,    -- Seg Offset (0)
+	0.01, -- Stiffness (0.01)
+	0.9,  -- Bounce (0.9)
+	0,    -- Fly Offset (0)
+	-20,  -- Down Limit (-20)
+	20    -- Up Limit (20)
+)
+
+-- Squishy vanilla arms
+local leftArm = squapi.arm:new(
+	parts.group.LeftArm,
+	1,     -- Strength (1)
+	false, -- Right Arm (false)
+	true   -- Keep Position (false)
+)
+
+local rightArm = squapi.arm:new(
+	parts.group.RightArm,
+	1,    -- Strength (1)
+	true, -- Right Arm (true)
+	true  -- Keep Position (false)
+)
+
+-- Arm strength variables
+local leftArmStrength  = leftArm.strength
+local rightArmStrength = rightArm.strength
+
+function events.TICK()
+	
+	-- Arm variables
+	local handedness  = player:isLeftHanded()
+	local activeness  = player:getActiveHand()
+	local leftActive  = not handedness and "OFF_HAND" or "MAIN_HAND"
+	local rightActive = handedness and "OFF_HAND" or "MAIN_HAND"
+	local leftSwing   = player:getSwingArm() == leftActive
+	local rightSwing  = player:getSwingArm() == rightActive
+	local leftItem    = player:getHeldItem(not handedness)
+	local rightItem   = player:getHeldItem(handedness)
+	local using       = player:isUsingItem()
+	local usingL      = activeness == leftActive and leftItem:getUseAction() or "NONE"
+	local usingR      = activeness == rightActive and rightItem:getUseAction() or "NONE"
+	local bow         = using and (usingL == "BOW" or usingR == "BOW")
+	local crossL      = leftItem.tag and leftItem.tag["Charged"] == 1
+	local crossR      = rightItem.tag and rightItem.tag["Charged"] == 1
+	
+	-- Arm movement overrides
+	local armShouldMove = pose.swim or pose.elytra or pose.crawl or pose.climb
+	
+	-- Control targets based on variables
+	leftArmLerp.target  = (armsMove or armShouldMove or leftSwing  or bow or ((crossL or crossR) or (using and usingL ~= "NONE"))) and 1 or 0
+	rightArmLerp.target = (armsMove or armShouldMove or rightSwing or bow or ((crossL or crossR) or (using and usingR ~= "NONE"))) and 1 or 0
+	
+end
+
+function events.RENDER(delta, context)
+	
+	-- Variables
+	local idleTimer   = world.getTime(delta)
+	local idleRot     = vec(math.deg(math.sin(idleTimer * 0.067) * 0.05), 0, math.deg(math.cos(idleTimer * 0.09) * 0.05 + 0.05))
+	local firstPerson = context == "FIRST_PERSON"
+	
+	-- Adjust arm strengths
+	leftArm.strength  = leftArmStrength  * leftArmLerp.currPos
+	rightArm.strength = rightArmStrength * rightArmLerp.currPos
+	
+	-- Adjust arm characteristics after applied by squapi
+	parts.group.LeftArm
+		:offsetRot(
+			parts.group.LeftArm:getOffsetRot()
+			+ ((-idleRot + (vanilla_model.BODY:getOriginRot() * 0.75)) * math.map(leftArmLerp.currPos, 0, 1, 1, 0))
+		)
+		:pos(parts.group.LeftArm:getPos() * vec(1, 1, -1))
+		:visible(not firstPerson)
+	
+	parts.group.RightArm
+		:offsetRot(
+			parts.group.RightArm:getOffsetRot()
+			+ ((idleRot + (vanilla_model.BODY:getOriginRot() * 0.75)) * math.map(rightArmLerp.currPos, 0, 1, 1, 0))
+		)
+		:pos(parts.group.RightArm:getPos() * vec(1, 1, -1))
+		:visible(not firstPerson)
+	
+	-- Set visible if in first person
+	--[[
+	parts.group.LeftArmFP:visible(firstPerson)
+	parts.group.RightArmFP:visible(firstPerson)
+	--]]
+	
+end
+
+-- Arm movement toggle
+function pings.setSquapiArmsMove(boolean)
+	
+	armsMove = boolean
+	config:save("SquapiArmsMove", armsMove)
+	
+end
+
+-- Sync variable
+function pings.syncSquapi(a)
+	
+	armsMove = a
+	
+end
+
+-- Host only instructions
+if not host:isHost() then return end
+
+-- Required scripts
+local itemCheck = require("lib.ItemCheck")
+local s, color = pcall(require, "scripts.ColorProperties")
+if not s then color = {} end
+
+-- Sync on tick
+function events.TICK()
+	
+	if world.getTime() % 200 == 0 then
+		pings.syncSquapi(armsMove)
+	end
+	
+end
+
+-- Table setup
+local t = {}
+
+-- Action
+t.armsAct = action_wheel:newAction()
+	:item(itemCheck("red_dye"))
+	:toggleItem(itemCheck("rabbit_foot"))
+	:onToggle(pings.setSquapiArmsMove)
+	:toggled(armsMove)
+
+-- Update action
+function events.RENDER(delta, context)
+	
+	if action_wheel:isEnabled() then
+		t.armsAct
+			:title(toJson
+				{"",
+				{text = "Arm Movement Toggle\n\n", bold = true, color = color.primary},
+				{text = "Toggles the movement swing movement of the arms.\nActions are not effected.", color = color.secondary}}
+			)
+		
+		for _, page in pairs(t) do
+			page:hoverColor(color.hover):toggleColor(color.active)
+		end
+		
+	end
+	
+end
+
+-- Return action
+return t
