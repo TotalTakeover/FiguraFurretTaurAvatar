e69339869c4c802409b5d00ff7b0205b84bfd8c3
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-12-09 22:12:21 -0700

    Arm code cleanup

--- scripts/SquishyAnims.lua
+++ scripts/SquishyAnims.lua
@@ -2,10 +2,8 @@
 local s, squapi = pcall(require, "lib.SquAPI")
 if not s then return {} end
 
--- Required scripts
+-- Required script
 local parts = require("lib.PartsAPI")
-local lerp  = require("lib.LerpAPI")
-local pose  = require("scripts.Posing")
 
 -- Animation setup
 local anims = animations.FurretTaur
@@ -13,7 +11,6 @@ local anims = animations.FurretTaur
 -- Config setup
 config:name("FurretTaur")
 local earFlick = config:load("SquapiEarFlick")
-local armsMove = config:load("SquapiArmsMove") or false
 if earFlick == nil then earFlick = true end
 
 -- Calculate parent's rotations
@@ -26,10 +23,6 @@ local function calculateParentRot(m)
 	return calculateParentRot(parent) + m:getOffsetRot()
 	
 end
-
--- Lerp tables
-local leftArmLerp  = lerp:new(armsMove and 1 or 0, 0.5)
-local rightArmLerp = lerp:new(armsMove and 1 or 0, 0.5)
 
 -- Squishy ears
 local ears = squapi.ear:new(
@@ -82,25 +75,6 @@ local head = squapi.smoothHead:new(
 	false -- Keep Original Head Pos (false)
 )
 
--- Squishy vanilla arms
-local leftArm = squapi.arm:new(
-	parts.group.LeftArm,
-	1,     -- Strength (1)
-	false, -- Right Arm (false)
-	true   -- Keep Position (true)
-)
-
-local rightArm = squapi.arm:new(
-	parts.group.RightArm,
-	1,    -- Strength (1)
-	true, -- Right Arm (true)
-	true  -- Keep Position (true)
-)
-
--- Arm strength variables
-local leftArmStrength  = leftArm.strength
-local rightArmStrength = rightArm.strength
-
 -- Squishy taur
 local taur = squapi.taur:new(
 	parts.group.LowerBody
@@ -108,66 +82,12 @@ local taur = squapi.taur:new(
 
 function events.TICK()
 	
-	-- Arm variables
-	local handedness  = player:isLeftHanded()
-	local activeness  = player:getActiveHand()
-	local leftActive  = not handedness and "OFF_HAND" or "MAIN_HAND"
-	local rightActive = handedness and "OFF_HAND" or "MAIN_HAND"
-	local leftSwing   = player:getSwingArm() == leftActive
-	local rightSwing  = player:getSwingArm() == rightActive
-	local leftItem    = player:getHeldItem(not handedness)
-	local rightItem   = player:getHeldItem(handedness)
-	local using       = player:isUsingItem()
-	local usingL      = activeness == leftActive and leftItem:getUseAction() or "NONE"
-	local usingR      = activeness == rightActive and rightItem:getUseAction() or "NONE"
-	local bow         = using and (usingL == "BOW" or usingR == "BOW")
-	local crossL      = leftItem.tag and leftItem.tag["Charged"] == 1
-	local crossR      = rightItem.tag and rightItem.tag["Charged"] == 1
-	
-	-- Arm movement overrides
-	local armShouldMove = pose.swim or pose.elytra or pose.crawl or pose.climb
-	
-	-- Control targets based on variables
-	leftArmLerp.target  = (armsMove or armShouldMove or leftSwing  or bow or ((crossL or crossR) or (using and usingL ~= "NONE"))) and 1 or 0
-	rightArmLerp.target = (armsMove or armShouldMove or rightSwing or bow or ((crossL or crossR) or (using and usingR ~= "NONE"))) and 1 or 0
-	taur.target         = (onGround or player:getVehicle() or anims.groundIdle:isPlaying()) and 0 or taur.target
-	
 	-- Control ear flick based on variables
 	ears.doEarFlick = earFlick
 	
 end
 
 function events.RENDER(delta, context)
-	
-	-- Variables
-	local idleTimer   = world.getTime(delta)
-	local idleRot     = vec(math.deg(math.sin(idleTimer * 0.067) * 0.05), 0, math.deg(math.cos(idleTimer * 0.09) * 0.05 + 0.05))
-	local firstPerson = context == "FIRST_PERSON"
-	
-	-- Adjust arm strengths
-	leftArm.strength  = leftArmStrength  * leftArmLerp.currPos
-	rightArm.strength = rightArmStrength * rightArmLerp.currPos
-	
-	-- Adjust arm characteristics after applied by squapi
-	parts.group.LeftArm
-		:offsetRot(
-			parts.group.LeftArm:getOffsetRot()
-			+ ((-idleRot + (vanilla_model.BODY:getOriginRot() * 0.75)) * math.map(leftArmLerp.currPos, 0, 1, 1, 0))
-		)
-		:pos(parts.group.LeftArm:getPos() * vec(1, 1, -1))
-		:visible(not firstPerson)
-	
-	parts.group.RightArm
-		:offsetRot(
-			parts.group.RightArm:getOffsetRot()
-			+ ((idleRot + (vanilla_model.BODY:getOriginRot() * 0.75)) * math.map(rightArmLerp.currPos, 0, 1, 1, 0))
-		)
-		:pos(parts.group.RightArm:getPos() * vec(1, 1, -1))
-		:visible(not firstPerson)
-	
-	-- Set visible if in first person
-	parts.group.LeftArmFP:visible(firstPerson)
-	parts.group.RightArmFP:visible(firstPerson)
 	
 	-- Set upperbody to offset rot and crouching pivot point
 	parts.group.UpperBody:rot(-parts.group.LowerBody:getRot())
@@ -190,19 +110,10 @@ function pings.setSquapiEarFlick(boolean)
 	
 end
 
--- Arm movement toggle
-function pings.setSquapiArmsMove(boolean)
-	
-	armsMove = boolean
-	config:save("SquapiArmsMove", armsMove)
-	
-end
-
--- Sync variables
-function pings.syncSquapi(a, b)
+-- Sync variable
+function pings.syncSquapi(a)
 	
 	earFlick = a
-	armsMove = b
 	
 end
 
@@ -213,7 +124,7 @@ if not host:isHost() then return end
 function events.TICK()
 	
 	if world.getTime() % 200 == 0 then
-		pings.syncSquapi(earFlick, armsMove)
+		pings.syncSquapi(earFlick)
 	end
 	
 end
@@ -246,12 +157,6 @@ a.earsAct = animsPage:newAction()
 	:onToggle(pings.setSquapiEarFlick)
 	:toggled(earFlick)
 
-a.armsAct = animsPage:newAction()
-	:item(itemCheck("red_dye"))
-	:toggleItem(itemCheck("rabbit_foot"))
-	:onToggle(pings.setSquapiArmsMove)
-	:toggled(armsMove)
-
 -- Update actions
 function events.RENDER(delta, context)
 	
@@ -269,15 +174,6 @@ function events.RENDER(delta, context)
 					"",
 					{text = "Ear Flick Toggle\n\n", bold = true, color = c.primary},
 					{text = "Toggles the ability for the ears to flick.", color = c.secondary}
-				}
-			))
-		
-		a.armsAct
-			:title(toJson(
-				{
-					"",
-					{text = "Arm Movement Toggle\n\n", bold = true, color = c.primary},
-					{text = "Toggles the movement swing movement of the arms.\nActions are not effected.", color = c.secondary}
 				}
 			))
 		
